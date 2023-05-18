c99a192058910fe7c98ab3825faf5509dcd6e49c
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-05-18 16:11:40 +0100

    Copyright updated

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
         { children }
       </div>
       <footer>
-        <p>Copyright 2023 @Reece</p>
+        <p>Copyright 2023 Reece</p>
       </footer>
     </div>
   )
