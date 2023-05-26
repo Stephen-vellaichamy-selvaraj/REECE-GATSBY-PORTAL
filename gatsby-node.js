204e769d5c2792ec760d9c3e86622b267f68dcc2
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-05-26 13:00:05 +0100

    Redirect Test Update 002

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,24 +10,24 @@ exports.createPages = async ({ graphql, actions }) => {
     statusCode: 200, force: true
   })
 
-  const { data } = await graphql(`
-    query BrandPage {
-      allContentfulPageBrands {
-        nodes {
-          brandsSelection {
-            slug
-          }
-        }
-      }
-    }
-  `)
+  // const { data } = await graphql(`
+  //   query BrandPage {
+  //     allContentfulPageBrands {
+  //       nodes {
+  //         brandsSelection {
+  //           slug
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  data.allContentfulPageBrands.nodes[0].brandsSelection.forEach(node => {
-    actions.createPage({
-      path: '/brands/'+ node.slug,
-      component: path.resolve('./src/templates/brand-details.js'),
-      context: { slug: node.slug }
-    })
-  })  
+  // data.allContentfulPageBrands.nodes[0].brandsSelection.forEach(node => {
+  //   actions.createPage({
+  //     path: '/brands/'+ node.slug,
+  //     component: path.resolve('./src/templates/brand-details.js'),
+  //     context: { slug: node.slug }
+  //   })
+  // })  
 
 }
