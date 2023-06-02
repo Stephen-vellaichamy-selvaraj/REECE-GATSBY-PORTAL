0ff54e0d0d0c47227ae14df6affff9a4850603f1
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-06-02 18:46:41 +0100

    lightning update

--- src/pages/test.js
+++ src/pages/test.js
@@ -6,22 +6,34 @@ function MyPage() {
   return(
     <Layout>
       <Script id="first-unique-id" dangerouslySetInnerHTML={{ __html: `alert('Hello world')` }} />
-      <Script src="https://reeceusa.my.salesforce.com/lightning/lightning.out.js"></Script>
-      
-      <Script> 
-          {/* $Lightning.use("runtime_appointmentbooking:lightningOutGuest",
-              function() {                  // Callback once framework and app load
-                  $Lightning.createComponent(
-                      "lightning:flow",    // top-level component of your app
-                      { },    // attributes to set on the component when created
-                      "lexcontainer",    // the DOM location to insert the component
-                      function(component) {            // API name of the Flow
-                          component.startFlow("Inbound_Custom_Guest_Appointment");
-                      }
-                  )
-              },    'https://reeceusa.my.salesforce-sites.com/'  // Site endpoint
-          ); */}
-      </Script> 
+      <script src="https://reeceusa.my.salesforce-sites.com/lightning/lightning.out.js"></script>
+      <script>
+
+    {/* $Lightning.use("runtime_appointmentbooking:lightningOutGuest",
+
+        function() {                  // Callback once framework and app load
+
+            $Lightning.createComponent(
+
+                "lightning:flow",    // top-level component of your app
+
+                { },    // attributes to set on the component when created
+
+                "lexcontainer",    // the DOM location to insert the component
+
+                function(component) {            // API name of the Flow
+
+                    component.startFlow("Inbound_Custom_Guest_Appointment");
+
+                }
+
+            )
+
+        },    'https://reeceusa.my.salesforce-sites.com/'  // Site endpoint
+
+    ); */}
+
+</script>
 
       <h4>Cancel Appointment</h4> 
  
