5c05f1afe50dbc79ec74283b0cc82cd822c4733c
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-06-02 17:24:29 +0100

    Index file update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
+// Home Page
 export default function Home({data}) {
   const image = getImage(data.allContentfulSiteSettings.edges[0].node.coverImage);
   return (
@@ -15,6 +16,7 @@ export default function Home({data}) {
           <p>Reece Business to customer website</p>
           <Link className={styles.btn} to="/brands">Brands</Link>
         </div>
+        <div id="lexcontainer">&nbsp;</div>
         <GatsbyImage image={image} alt="blog image" layout="fixed" width={1000} height={1000}/>
       </section>
     </Layout>
