8c4c2dd93eea13c7ded606ae49efbc7005fd44c0
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-06-02 19:08:05 +0100

    lexcontainer

--- src/pages/test.js
+++ src/pages/test.js
@@ -5,47 +5,24 @@ import Layout from '../components/Layout'
 function MyPage() {
   return(
     <Layout>
-      <Script id="first-unique-id" dangerouslySetInnerHTML={{ __html: `alert('Hello world')` }} />
-      <script src="https://reeceusa.my.salesforce-sites.com/lightning/lightning.out.js"></script>
-      <script>
+      <div id="lexcontainer">Test</div>
+      <script src="https://reeceusa.my.salesforce.com/lightning/lightning.out.js"></script>
+      <script> 
+        $Lightning.use("runtime_appointmentbooking:lightningOutGuest",
+            function() {                  // Callback once framework and app load
+                $Lightning.createComponent(
+                    "lightning:flow",    // top-level component of your app
+                    { },    // attributes to set on the component when created
+                    "lexcontainer",    // the DOM location to insert the component
+                    function(component) {            // API name of the Flow
+                        component.startFlow("Inbound_Custom_Guest_Appointment");
+                    }
+                )
+            },    'https://reeceusa.my.salesforce-sites.com/'  // Site endpoint
+        );
+      </script>
 
-    {/* $Lightning.use("runtime_appointmentbooking:lightningOutGuest",
 
-        function() {                  // Callback once framework and app load
-
-            $Lightning.createComponent(
-
-                "lightning:flow",    // top-level component of your app
-
-                { },    // attributes to set on the component when created
-
-                "lexcontainer",    // the DOM location to insert the component
-
-                function(component) {            // API name of the Flow
-
-                    component.startFlow("Inbound_Custom_Guest_Appointment");
-
-                }
-
-            )
-
-        },    'https://reeceusa.my.salesforce-sites.com/'  // Site endpoint
-
-    ); */}
-
-</script>
-
-      <h4>Cancel Appointment</h4> 
- 
-      <div id="lexcontainercancel">&nbsp;</div>
-      <br />
-      <br />
-      <br />
-      &nbsp;
-      <h4>Modify Appointment</h4>
-  
-      <div id="lexcontainermodify">&nbsp;</div> 
- 
     </Layout>
   )
 }
